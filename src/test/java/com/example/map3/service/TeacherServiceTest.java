67ce0ac64df5d69613508d8e0f09ce8b5c93dbab
sunny <sunnyansal44@gmail.com>
2021-07-16 17:07:55 +0530

    test

---
+++ src/test/java/com/example/map3/service/TeacherServiceTest.java
@@ -0,0 +1,30 @@
+package com.example.map3.service;
+
+import com.example.map3.dao.TeacherDao;
+import com.example.map3.entities.Teacher;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+public class TeacherServiceTest {
+
+    @Autowired
+    private TeacherService teacherService;
+
+    @MockBean
+    private TeacherDao teacherDao;
+
+    @Test
+    public void addTeacherTest(){
+        Teacher teacher = new Teacher();
+        teacher.setName("sam");
+        teacher.setEmail("sam@email.com");
+
+        Mockito.when(teacherDao.save(teacher)).thenReturn(teacher);
+        Assertions.assertEquals(teacher,teacherService.addTeacher(teacher));
+    }
+}
